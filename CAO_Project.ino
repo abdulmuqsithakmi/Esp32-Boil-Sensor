9b0e0d40a2c14e997bee89fc9a4105ef6e250465
abdulmuqsithakmi <93818978+abdulmuqsithakmi@users.noreply.github.com>
2022-05-24 14:40:09 +0500

    Cao Project Wifi

--- CAO_Project.ino
+++ CAO_Project.ino
@@ -1,16 +1,24 @@
+#include<WiFi.h>
+#include<HTTPClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 // GPIO where the DS18B20 is connected to
 const int oneWireBus = 4; 
 
+const char* ssid     = "Redmi 9";
+const char* password = "muqsit19";
+
+const char* serverName = "https://maker.ifttt.com/trigger/boil/with/key/ny0_SyEvlquuIfxWGVIMPtmDAVeNea9_1JTiG7K_Wje";
+
+
     int ld = 18;
     int buzz=19;
 
-// Setup a oneWire instance to communicate with any OneWire devices
+
 OneWire oneWire(oneWireBus);
 
-// Pass our oneWire reference to Dallas Temperature sensor 
+
 DallasTemperature sensors(&oneWire);
 
 void setup() {
@@ -22,19 +30,50 @@ void setup() {
   // Start the DS18B20 sensor
   sensors.begin();
 
+  
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting...");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected to :");
+    Serial.println(ssid);
+    Serial.println("IP : ");
+    Serial.println(WiFi.localIP());
+
+
 }
 
 void loop() {
   sensors.requestTemperatures(); 
+  
   float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
+  
   Serial.print(temperatureC);
   Serial.println("ºC");
-  Serial.print(temperatureF);
-  Serial.println("ºF");
+  
   delay(1000);
 
-  if(temperatureC>50){
+  if(temperatureC>31){
+
+    HTTPClient http;
+
+      http.begin(serverName);
+
+      http.addHeader("Content-Type", "application/json");
+      String httpRequestData = "{\"value1\":\"The Milk is Boiled\"}";
+      int httpResponseCode = http.POST(httpRequestData);
+
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+      http.end();
 
     while(1){
     digitalWrite(ld,HIGH);
